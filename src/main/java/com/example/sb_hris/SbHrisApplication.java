835fb5e08b02e618248dcbef4535570a68c08e90
try-kode <leangtrysam656@gmail.com>
2025-08-01 19:49:51 +0700

    init project

---
+++ src/main/java/com/example/sb_hris/SbHrisApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb_hris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbHrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbHrisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sb_hris/SbHrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb_hris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbHrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
